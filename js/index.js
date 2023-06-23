ca8267d854ea85c9c5e3d98fcd4db0666871a078
Sasha Lomako <aleks.lomako@yandex.ru>
2023-06-23 09:50:47 +0300

    last commit 23/06/23

--- js/index.js
+++ js/index.js
@@ -7,17 +7,21 @@ const profileSubtitleElement = document.querySelector('.profile__subtitle');
 let nameInput = formElement.querySelector('input[name="name"]');
 let jobInput = formElement.querySelector('input[name="job"]');
 
-function handleClick() {
+function toggle(){
     popupElement.classList.toggle('popup_opened');
+}
+
+function handleClick() {
+    toggle();
     nameInput.value = profileTitleElement.textContent;
     jobInput.value = profileSubtitleElement.textContent;
 }
 
 function formSubmitHandler (evt) {
+    evt.preventDefault();
+    toggle();
     profileTitleElement.textContent = nameInput.value;
     profileSubtitleElement.textContent = jobInput.value;
-    popupElement.classList.toggle('popup_opened');
-	evt.preventDefault();
 }
 
 aboutButtonElement.addEventListener('click', handleClick);
